c0389d967d9a24c9ae14b4495cc9caac1a77e2b7
k2foxspb <k2foxpsb@mail.ru>
2023-03-14 20:42:26 +0300

    default renderer classes

--- config/config/settings.py
+++ config/config/settings.py
@@ -131,3 +131,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000'
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
