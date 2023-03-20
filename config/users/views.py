cbfb0f03199a0433b661a6300f2318caf6a6cf1f
k2foxspb <k2foxpsb@mail.ru>
2023-03-20 17:51:22 +0300

    versions in DRF AcceptHeaderVersioning

--- config/users/views.py
+++ config/users/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import BasePermission
 
 from users.models import Users
-from users.serializers import UsersSerializer
+from users.serializers import UsersSerializer, UserSerializerBase
 
 
 class StaffOnly(BasePermission):
@@ -14,3 +14,8 @@ class UsersModelViewSet(ModelViewSet):
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
     permission_classes = [StaffOnly]
+
+    def get_serializer_class(self):
+        if self.request.version == '0.2':
+            return UsersSerializer
+        return UserSerializerBase
